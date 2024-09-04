55002a9e5499d7b65b0b9a2c49481964803b4494
GorbunovGHub <pavlixrus@mail.ru>
2024-09-05 00:38:30 +0300

    Add files via upload

---
+++ module_16_4.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, HTTPException, Path
+from pydantic import BaseModel
+from typing import List, Annotated
+
+app = FastAPI()
+
+users = []
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+
+@app.get('/users')
+def get_users() -> List[User]:
+    return users
+
+
+@app.post('/user/{username}/{age}')
+def create_user(user: User, username: Annotated[str, Path(min_length=4, max_length=20, description='Enter username'
+    , example='Urban User')], age: int = Path(ge=18, le=70, description='Enter age, example=18')) -> str:
+    if users:
+        current_index = max(user.id for user in users) + 1
+    else:
+        current_index = 1
+    user.id = current_index
+    user.username = username
+    user.age = age
+    users.append(user)
+    return f'User {current_index} is registered.'
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+def update_user(user: User, user_id: int = Path(ge=0), username: str = Path(min_length=4, max_length=20
+    , description='Enter username', example='Urban User'), age: int = Path(ge=18
+    , le=70, description='Enter age', example=18)) -> str:
+    for i in users:
+        if i.id == user_id:
+            try:
+                i.username = username
+                i.age = age
+                return f'User {user_id} has been updated.'
+            except IndexError:
+                raise HTTPException(status_code=404, detail='User was not found')
+
+
+@app.delete('/user/{user_id}')
+def delete_user(user_id: int) -> str:
+    for index, i in enumerate(users):
+        if i.id == user_id:
+            users.pop(index)
+            return f'User ID {user_id} deleted'
+    raise HTTPException(status_code=404, detail='User was not found.')
